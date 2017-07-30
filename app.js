0d352bc8ffcb701df428709de091997a7cffd7b6
Jeremy Miller <jgmiller88@gmail.com>
2017-07-29 17:41:17 -0700

    separate out functionality

--- app.js
+++ app.js
@@ -2,15 +2,21 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res) {
+app.get('/', defaultRoute);
+
+http.listen(3000, listen);
+
+io.on('connection', socketConnection);
+
+function defaultRoute(req, res) {
   res.sendFile(__dirname + '/public/view/index.html');
-});
+}
 
-http.listen(3000, function() {
+function listen() {
   console.log('listening on *:3000');
-});
+}
 
-io.on('connection', function(socket) {
+function socketConnection(socket) {
   console.log('a user connected');
   socket.on('chat message', function(msg) {
     io.emit('chat message', msg);
@@ -18,4 +24,4 @@ io.on('connection', function(socket) {
   socket.on('disconnect', function() {
     console.log('a user disconnected');
   });
-});
+}
