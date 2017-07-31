8fdad75ca9d3b1e44f7b4d7ae4d1d6ee164808e8
Jeremy Miller <jgmiller88@gmail.com>
2017-07-30 17:12:44 -0700

    change single quotes to double

--- static/js/socket.js
+++ static/js/socket.js
@@ -2,24 +2,24 @@
 
 var socket = io();
 
-$('form').submit(function() {
-  socket.emit('chat message', $('#message').val());
-  $('#message').val('');
+$("form").submit(function() {
+  socket.emit("chat message", $("#message").val());
+  $("#message").val("");
   return false;
 });
 
-socket.on('chat message', chatMessage);
-socket.on('user connected', userConnected);
-socket.on('user disconnected', userDisconnected);
+socket.on("chat message", chatMessage);
+socket.on("user connected", userConnected);
+socket.on("user disconnected", userDisconnected);
 
 function chatMessage(msg) {
-  $('#messages').append($('<li>').text(msg));
+  $("#messages").append($("<li>").text(msg));
 }
 
 function userConnected() {
-  $('#messages').append($('<li>').text('--- user connected ---'));
+  $("#messages").append($("<li>").text("--- user connected ---"));
 }
 
 function userDisconnected() {
-  $('#messages').append($('<li>').text('--- user disconnected ---'));
+  $("#messages").append($("<li>").text("--- user disconnected ---"));
 }
